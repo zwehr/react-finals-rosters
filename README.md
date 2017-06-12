41e81628f57855d49d72fb049cfd1a103a30937a
zwehr <zwehrmeister@gmail.com>
2017-06-11 23:42:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Interactive 2017 NBA Finals Rosters.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>2017 NBA Finals Rosters</h1>
+        <h2>Preview matchups and see stats.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
