73c8fb827d6d2874548d30d79ddcded632232e1e
zwehr <zwehrmeister@gmail.com>
2017-06-13 16:26:34 -0400

    add index position state to PlayerCard component

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -3,10 +3,17 @@ import PlayerPhoto from './PlayerPhoto';
 import PlayerInfo from './PlayerInfo';
 
 class PlayerCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerIndex: 0
+    }
+  }
+
   render() {
     return (
       <div className="playerCard">
-        from PlayerCard component, rendering:
+        from PlayerCard component, state is {this.state.playerIndex} rendering:
         <PlayerPhoto />
         <PlayerInfo />
       </div>
