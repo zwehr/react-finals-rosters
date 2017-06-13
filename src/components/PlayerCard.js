8a73f29a89f426f977367b5cf3c6d9f68a9fe71d
zwehr <zwehrmeister@gmail.com>
2017-06-13 19:01:33 -0400

    add onClick functions for back/next arrows

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -10,17 +10,46 @@ class PlayerCard extends Component {
     };
   }
 
+  handleClickBack() {
+    let currentIndex = this.state.playerIndex;
+    if (currentIndex === 0) {
+      this.setState({
+        playerIndex: 14
+      });
+    } else {
+      this.setState({
+        playerIndex: --currentIndex
+      })
+    }
+  }
+
+  handleClickForward() {
+    let currentIndex = this.state.playerIndex;
+    if (currentIndex === 14) {
+      this.setState({
+        playerIndex: 0
+      });
+    } else {
+      this.setState({
+        playerIndex: ++currentIndex
+      });
+    }
+  }
+
   render() {
-    let playerName = this.props.roster[this.state.playerIndex].name;
-    let playerPhoto = this.props.roster[this.state.playerIndex].img;
-    let playerNumber = this.props.roster[this.state.playerIndex].number;
-    let playerPosition = this.props.roster[this.state.playerIndex].position;
+    let player = this.props.roster[this.state.playerIndex];
+
+    let playerName = player.name;
+    let playerPhoto = player.img;
+    let playerNumber = player.number;
+    let playerPosition = player.position;
+
     return (
       <div className="playerCard">
         <PlayerPhoto photo={playerPhoto} name={playerName} />
         <div className="arrows">
-          <i className="fa fa-arrow-left" aria-hidden="true"></i>
-          <i className="fa fa-arrow-right" aria-hidden="true"></i>
+          <i onClick={this.handleClickBack.bind(this)} className="fa fa-arrow-left" aria-hidden="true"></i>
+          <i onClick={this.handleClickForward.bind(this)} className="fa fa-arrow-right" aria-hidden="true"></i>
         </div>
         <PlayerInfo name={playerName} number={playerNumber} position={playerPosition}/>
       </div>
